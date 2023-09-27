2e4f1f0381c3e0cd6fedfa0aa080e5f8c4ee61f2
Tamjid Ahmed <tamjidahammad10@gmail.com>
2023-09-28 00:15:54 +0600

    ss

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,15 @@ export default function Home() {
 		<Projects/>
 		<BlogPosts/>
 		<ContactMe/>
+		<div className="dark dark:bg-gray-800 dark:text-white bg-white text-black">
+  ...
+  <div>Text color changes based on theme</div>
+</div>
+
+<div className="light light:bg-gray-100 light:text-black bg-black text-white">
+  ...
+  <div>Text color changes based on theme</div>
+</div>
 	</>
 	);
 }
--- components/portfolio/ContactMe.portfolio.tsx
+++ components/portfolio/ContactMe.portfolio.tsx
@@ -6,7 +6,7 @@ const ContactMe =()=>{
         <form action="#" className="space-y-8">
             <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium ">Your email</label>
-                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
+                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
             </div>
             <div>
                 <label htmlFor="subject" className="block mb-2 text-sm font-medium ">Subject</label>
@@ -19,6 +19,7 @@ const ContactMe =()=>{
             <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
         </form>
     </div>
+
   </section>)
 
 }

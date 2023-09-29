4e2e06e3f1e6394f230d7525d98f37423dddef76
Tamjid Ahmed <tamjidahammad10@gmail.com>
2023-09-30 00:04:52 +0600

    fixed experiences

--- components/portfolio/Experience.portfolio.tsx
+++ components/portfolio/Experience.portfolio.tsx
@@ -1,87 +1,35 @@
+import { siteConfig } from "@/config/site"
 const Experience = () => {
-    return (<div className="container mx-auto max-w-7xl">
+    return (<div className="container mx-auto max-w-7xl px-10 md:px-4">
     <h1 className="font-bold  mb-10 text-3xl text-center">Experiences</h1>
 
     <div className="relative mt-5 text-left md:grid md:grid-cols-3 md:gap-12">
         
-        
-        <div className="flex items-center relative">
+        {siteConfig.experience.map((item,index)=>{
+            return<div className="flex items-center relative">
             <div className="hidden md:block w-20">
-                <div className="font-bold italic">2020</div>
+                <div className="font-bold italic">{item.year}</div>
                 <div className="md:flex space-x-1 text-xs">
-                    <div>Abr.</div>
+                    <div>{item.startMonth}</div>
                     <div>-</div>
-                    <div>Jun.</div>
+                    <div>{item.endMonth}</div>
                 </div>                        
             </div>
             
-            <div className="border-r-2 border-black absolute h-full md:h-4/5  left-1 md:left-20 top-2 md:top-auto z-[10]">
-                <div className="absolute w-5 h-5 rounded-full bg-black -top-1 md:top-[50%] transform translate-y-[-50%]  -ml-2 "></div>
+            <div className="border-r-2 border-black dark:border-slate-100 absolute h-full md:h-4/5  left-1 md:left-20 top-2 md:top-auto z-[10]">
+                <div className="absolute w-5 h-5 rounded-full bg-black dark:bg-slate-100 -top-1 md:top-[50%] transform translate-y-[-50%]  -ml-2 "></div>
             </div>
-
             <div className="ml-10">
-                <div className="font-bold">Proyecto 1</div>
-                <div className="italic md:mb-4">Empresa 1</div>
+                <div className="font-bold">{item.companyName}</div>
+                <div className="italic md:mb-4">{item.postion}</div>
                 <div className="mb-4 mt-2 md:hidden">
-                    <div className="font-bold">2020</div>
-                    <div className="text-xs">Abril - Junio</div>
+                    <div className="font-bold">{item.year}</div>
+                    <div className="text-xs">{item.startMonth} - {item.endMonth}</div>
                 </div>
-                <div className="mb-10">Fusce auctor gravida dui.</div>
+                <div className="mb-10 text-justify">{item.jobDescription}</div>
             </div>
         </div>
-
-        <div className="flex items-center relative">
-            <div className="hidden md:block w-20">
-                <div className="font-bold italic">2020</div>
-                <div className="md:flex space-x-1 text-xs">
-                    <div>Abr.</div>
-                    <div>-</div>
-                    <div>Jun.</div>
-                </div>                        
-            </div>
-            
-            <div className="border-r-2 border-black absolute h-full md:h-4/5  left-1 md:left-20 top-2 md:top-auto z-[10]">
-                <div className="absolute w-5 h-5 rounded-full bg-black -top-1 md:top-[50%] transform translate-y-[-50%]  -ml-2 "></div>
-            </div>
-
-            <div className="ml-10">
-                <div className="font-bold">Proyecto 1</div>
-                <div className="italic md:mb-4">Empresa 1</div>
-                <div className="mb-4 mt-2 md:hidden">
-                    <div className="font-bold">2020</div>
-                    <div className="text-xs">Abril - Junio</div>
-                </div>
-                <div className="mb-10">Fusce auctor gravida dui.</div>
-            </div>
-        </div>
-
-        <div className="flex items-center relative">
-            <div className="hidden md:block w-20">
-                <div className="font-bold italic">2020</div>
-                <div className="md:flex space-x-1 text-xs">
-                    <div>Abr.</div>
-                    <div>-</div>
-                    <div>Jun.</div>
-                </div>                        
-            </div>
-            
-            <div className="border-r-2 border-black absolute h-full md:h-4/5  left-1 md:left-20 top-2 md:top-auto z-[10]">
-                <div className="absolute w-5 h-5 rounded-full bg-black -top-1 md:top-[50%] transform translate-y-[-50%]  -ml-2 "></div>
-            </div>
-
-            <div className="ml-10">
-                <div className="font-bold">Proyecto 1</div>
-                <div className="italic md:mb-4">Empresa 1</div>
-                <div className="mb-4 mt-2 md:hidden">
-                    <div className="font-bold">2020</div>
-                    <div className="text-xs">Abril - Junio</div>
-                </div>
-                <div className="mb-10">Fusce auctor gravida dui.</div>
-            </div>
-        </div>
-       
-        
-
+        })}
     </div>
 </div>)
 }
--- config/site.ts
+++ config/site.ts
@@ -78,5 +78,26 @@ export const siteConfig = {
 			skill:"React"
 		},]
 	},
-	
+	experience:[{
+		year:"2022",
+		startMonth:"Jan",
+		endMonth:"Apr",
+		companyName:"Tau Research Ltd.",
+		postion:"Inten",
+		jobDescription:"Built and improved a React dashboard for data visualization and analysis, using custom components, hooks, APIs, and best practices. Also worked with AWS EC2 services."
+	},{
+		year:"2022",
+		startMonth:"Apr",
+		endMonth:"Jun ",
+		companyName:"Digital Gregg LLC. ",
+		postion:"React Developer",
+		jobDescription:"Enhanced a admin dashboard using React, Tailwind CSS, and Next.js, optimizing UI with Tailwind, custom components, React hooks, and API integrations." 
+	},{
+		year:"2022",
+		startMonth:"Jun",
+		endMonth:"Now",
+		companyName:"One Direction Companies Limited",
+		postion:"React Developer",
+		jobDescription:"Designed captivating animations for Walton Digitech, crafted an elegant e-commerce frontend with Material UI and ReactJS, and developed a feature-rich Stockmate website."
+	},]
 };

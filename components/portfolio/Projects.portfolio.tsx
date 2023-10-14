d9d591f7aed5bb97686c0bd327763e10d36e0e86
Tamjid Ahmed <tamjidahammad10@gmail.com>
2023-10-15 00:04:47 +0600

    sssss

--- components/portfolio/Projects.portfolio.tsx
+++ components/portfolio/Projects.portfolio.tsx
@@ -9,7 +9,7 @@ const ProjectCard = () => {
         <Card className="py-4 w-[fit-content]">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <p className="text-tiny uppercase font-bold">Daily Mix</p>
-                <small className="text-default-500">12 Tracks</small>
+                <small className="text-default-500">12 Tracksss</small>
                 <h4 className="font-bold text-large">Frontend Radio</h4>
             </CardHeader>
             <CardBody className="overflow-visible py-2">
